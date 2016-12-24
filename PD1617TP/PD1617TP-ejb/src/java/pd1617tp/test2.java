e7bdc72bd0bb643eee90fcdc4077a3b548032320
Pedro Salgado <pmmslive@gmail.com>
2016-12-24 13:05:53 +0000

    blanc project

---
+++ PD1617TP/PD1617TP-ejb/src/java/pd1617tp/test2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pd1617tp;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Pedro Salgado
+ */
+@Stateless
+@LocalBean
+public class test2 {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
