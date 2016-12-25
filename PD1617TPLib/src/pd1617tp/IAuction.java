f71ca73ac6f82da6c4373179938c43d21a208b7e
Pedro Salgado <pmmslive@gmail.com>
2016-12-25 19:05:03 +0000

    add lib folder

---
+++ PD1617TPLib/src/pd1617tp/IAuction.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pd1617tp;
+
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Pedro Salgado
+ */
+@LocalBean
+public interface IAuction {
+     boolean LoginUser(String Username, String Password);
+}
---
+++ PD1617TPLib/src/pd1617tp/IUser.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pd1617tp;
+
+import java.util.ArrayList;
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author Pedro Salgado
+ */
+@Remote
+public interface IUser {
+    
+    boolean Login(String name, String password);
+    boolean LogOff();
+    
+}
